49c4d97c830e61164f5d4a44e5e5766ff0917da9
kattaakhil <39687976+kattaakhil@users.noreply.github.com>
2019-07-19 14:56:09 +0530

    Add files via upload

---
+++ Adamnumber.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include<math.h>
+int rev(int n)
+{
+    int rem,result=0;
+    while(n)
+    {
+        rem=n%10;
+        result=result*10+rem;
+        n=n/10;
+    }
+    return result;
+}
+int main()
+{
+    int n,temp,a,b,c,result;
+    scanf("%d",&n);
+    temp=n;
+    a=n*n;
+    b=rev(a);
+    c=sqrt(b);
+    result=rev(c);
+    if(result==temp)
+    {
+        printf("Adam Number");
+    }
+    else
+    {
+        printf("Not an Adam Number");
+    }
+    return 0;
+}
